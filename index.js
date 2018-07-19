677d087e98874653a1324651bc196e8fbb11f458
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 14:57:21 -0300

    project starter

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src';
+
+import './src/config/ReactotronConfig';
+import './src/config/DevToolsConfig';
 
 AppRegistry.registerComponent('Desafio01', () => App);
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+import styles from './styles';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hello world
+        </Text>
+      </View>
+    );
+  }
+}
